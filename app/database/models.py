d6caf64c5157a0bc16080dbd999333d3616356cf
hafidzduddin <hafidzduddin85@gmail.com>
2025-07-29 01:23:28 +0700

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -20,3 +20,4 @@ class Profile(Base):
     is_active = Column(Boolean, nullable=False, default=True)
     photo_url = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
